deb6c4448fcf90550f2c2936196b430ca827225f
Tom Hilton <tomhilton93@gmail.com>
2023-03-25 22:37:58 +0000

    Run associated test file if not currently open

--- README.md
+++ README.md
@@ -2,15 +2,17 @@
 
 ## Install
 
-Download the .vsix from the releases page
+Download the latest .vsix from the releases page
 
 ```sh
-code --install-extension filename.vsix
+code --install-extension toms-tools-X.X.X.vsix
 ```
 
 ## Commands
 
-### toggle comment style
+All commands are prefixed `TOM:`
+
+### Toggle comment style
 
 `Toggle comment style`: Converts block comments to line comments
 
@@ -28,14 +30,14 @@ to
 // world
 ```
 
-### toggle test file
+### Toggle test file
 
 `Toggle test file`: Switches between `*.spec.ts` file and its implementation file. Creates the test file if it doesn't exist.
 
-### run test file
+### Run test file
 
-`Run test file`: Opens a terminal, cd's to the correct workspace directory and runs `yarn unit` for the current open `*.spec.ts` file.
+`Run test file`: Opens a terminal, cd's to the correct workspace directory and runs `yarn unit` for the current open `*.spec.ts` file. If not in a test file, it will look for a `*.spec.ts` file that has the same name as the current open file and runs `yarn unit`.
 
-### run test file and watch
+### Run test file and watch
 
 `Run test file and watch`: The same as "run test file" but adds `--watch` to the end of the command.
--- src/runTestFile.ts
+++ src/runTestFile.ts
@@ -1,25 +1,35 @@
 import * as vscode from 'vscode'
 
-export default function runTestFile(watch: boolean = false) {
+export default async function runTestFile(watch: boolean = false) {
   const editor = vscode.window.activeTextEditor
   if (editor) {
+    let fileUri = editor.document.uri
     const fileName = editor.document.fileName
-    editor.document.uri
     const fileNameParts = fileName.split('/')
     const file = fileNameParts.pop() || ''
 
     if (!file.endsWith('.spec.ts')) {
-      // For now don't do anything else
-      vscode.window.showInformationMessage(`This isn't a test file`)
-      return
+      // Switch to the test file
+      const folder = fileNameParts.join('/')
+      const fileNameWithoutExtension = file.split('.').shift() || ''
+      const pattern = new vscode.RelativePattern(folder, `${fileNameWithoutExtension}.spec.ts`)
+
+      const vals = await vscode.workspace.findFiles(pattern)
+      if (vals.length > 0) {
+        fileUri = vals[0]
+      } else {
+        vscode.window.showInformationMessage('Could not find a suitable test file to run')
+        return
+      }
     }
 
     const projectIndex = fileName.lastIndexOf('/src/')
     if (projectIndex === -1) {
       return
     }
+
     const projectPath = fileName.slice(0, projectIndex)
-    const relativePath = vscode.workspace.asRelativePath(editor.document.uri)
+    const relativePath = vscode.workspace.asRelativePath(fileUri)
 
     const terminal = vscode.window.activeTerminal || vscode.window.createTerminal('Terminal')
     terminal.show()
