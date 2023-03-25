e44696f8f586b0d1ba1e0026c664726e4b0c675c
Tom Hilton <tomhilton93@gmail.com>
2023-03-25 19:51:53 +0000

    Fix lint errors

--- src/extension.ts
+++ src/extension.ts
@@ -1,9 +1,9 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
-import runTestFile from './runTestFile';
-import toggleCommentStyle from './toggleCommentStyle';
-import toggleTestFile from './toggleTestFile';
+import * as vscode from 'vscode'
+import runTestFile from './runTestFile'
+import toggleCommentStyle from './toggleCommentStyle'
+import toggleTestFile from './toggleTestFile'
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -11,15 +11,15 @@ export function activate(context: vscode.ExtensionContext) {
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
-  const toggleCommentSub = vscode.commands.registerCommand('toms-tools.toggleCommentStyle', () => toggleCommentStyle());
-  const toggleTestSub = vscode.commands.registerCommand('toms-tools.toggleTestFile', () => toggleTestFile());
-  const runTestSub = vscode.commands.registerCommand('toms-tools.runTestFile', () => runTestFile());
-  const runTestWatchSub = vscode.commands.registerCommand('toms-tools.runTestFileWatch', () => runTestFile(true));
+  const toggleCommentSub = vscode.commands.registerCommand('toms-tools.toggleCommentStyle', () => toggleCommentStyle())
+  const toggleTestSub = vscode.commands.registerCommand('toms-tools.toggleTestFile', () => toggleTestFile())
+  const runTestSub = vscode.commands.registerCommand('toms-tools.runTestFile', () => runTestFile())
+  const runTestWatchSub = vscode.commands.registerCommand('toms-tools.runTestFileWatch', () => runTestFile(true))
 
-  context.subscriptions.push(toggleCommentSub);
-  context.subscriptions.push(toggleTestSub);
-  context.subscriptions.push(runTestSub);
-  context.subscriptions.push(runTestWatchSub);
+  context.subscriptions.push(toggleCommentSub)
+  context.subscriptions.push(toggleTestSub)
+  context.subscriptions.push(runTestSub)
+  context.subscriptions.push(runTestWatchSub)
 }
 
 // this method is called when your extension is deactivated
--- src/toggleCommentStyle.ts
+++ src/toggleCommentStyle.ts
@@ -1,82 +1,82 @@
-import * as vscode from 'vscode';
+import * as vscode from 'vscode'
 
 export default function toggleCommentStyle() {
-  const editor = vscode.window.activeTextEditor;
+  const editor = vscode.window.activeTextEditor
   if (editor) {
-    const document = editor.document;
-    const selection = editor.selection;
-    const currentLineNumber = selection.active.line;
-    const { text } = document.lineAt(currentLineNumber);
-    const lines: Line[] = [{ lineNumber: currentLineNumber, text }];
-    const textTrimmed = text.trim();
+    const document = editor.document
+    const selection = editor.selection
+    const currentLineNumber = selection.active.line
+    const { text } = document.lineAt(currentLineNumber)
+    const lines: Line[] = [{ lineNumber: currentLineNumber, text }]
+    const textTrimmed = text.trim()
 
     if (textTrimmed.startsWith('/**')) {
-      lines.push(...getLinesBelow(currentLineNumber + 1, document));
+      lines.push(...getLinesBelow(currentLineNumber + 1, document))
     } else if (textTrimmed.startsWith('*/')) {
-      lines.push(...getLinesAbove(currentLineNumber - 1, document));
+      lines.push(...getLinesAbove(currentLineNumber - 1, document))
     } else if (textTrimmed.startsWith('*')) {
-      lines.push(...getLinesBelow(currentLineNumber + 1, document));
-      lines.push(...getLinesAbove(currentLineNumber - 1, document));
+      lines.push(...getLinesBelow(currentLineNumber + 1, document))
+      lines.push(...getLinesAbove(currentLineNumber - 1, document))
     } else {
       // not a comment, return
-      return;
+      return
     }
 
     editor.edit((editBuilder) => {
       lines.forEach((line) => {
         if (line.text.trim().startsWith('/*') || line.text.trim().startsWith('*/')) {
-          const range = document.lineAt(line.lineNumber).rangeIncludingLineBreak;
-          editBuilder.delete(range);
+          const range = document.lineAt(line.lineNumber).rangeIncludingLineBreak
+          editBuilder.delete(range)
         } else {
-          const range = document.lineAt(line.lineNumber).range;
+          const range = document.lineAt(line.lineNumber).range
 
-          const lineText = line.text.replace(' *', '//');
-          editBuilder.replace(range, lineText);
+          const lineText = line.text.replace(' *', '//')
+          editBuilder.replace(range, lineText)
         }
-      });
-    });
+      })
+    })
   }
 }
 
 interface Line {
-  lineNumber: number;
-  text: string;
+  lineNumber: number
+  text: string
 }
 
 function getLinesAbove(startingLineNumber: number, document: vscode.TextDocument): Line[] {
-  const lines: Line[] = [];
-  let foundTop = false;
-  let lineNumber = startingLineNumber;
+  const lines: Line[] = []
+  let foundTop = false
+  let lineNumber = startingLineNumber
 
   while (!foundTop) {
-    const { text } = document.lineAt(lineNumber);
-    const textTrimmed = text.trim();
+    const { text } = document.lineAt(lineNumber)
+    const textTrimmed = text.trim()
 
-    lines.push({ text, lineNumber });
+    lines.push({ text, lineNumber })
     if (textTrimmed.startsWith('/**')) {
-      foundTop = true;
+      foundTop = true
     }
-    lineNumber--;
+    lineNumber--
   }
 
-  return lines;
+  return lines
 }
 
 function getLinesBelow(startingLineNumber: number, document: vscode.TextDocument): Line[] {
-  const lines: Line[] = [];
-  let foundBottom = false;
-  let lineNumber = startingLineNumber;
+  const lines: Line[] = []
+  let foundBottom = false
+  let lineNumber = startingLineNumber
 
   while (!foundBottom) {
-    const { text } = document.lineAt(lineNumber);
-    const textTrimmed = text.trim();
+    const { text } = document.lineAt(lineNumber)
+    const textTrimmed = text.trim()
 
-    lines.push({ text, lineNumber });
+    lines.push({ text, lineNumber })
     if (textTrimmed.startsWith('*/')) {
-      foundBottom = true;
+      foundBottom = true
     }
-    lineNumber++;
+    lineNumber++
   }
 
-  return lines;
+  return lines
 }
