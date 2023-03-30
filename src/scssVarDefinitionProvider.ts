fb9121b952ae095b2c8c55342ede91336a375196
Tom Hilton <tomhilton93@gmail.com>
2023-03-30 19:39:01 +0100

    Fix navigating to variables from another scss file

--- src/scssVarDefinitionProvider.ts
+++ src/scssVarDefinitionProvider.ts
@@ -12,9 +12,12 @@ export default class ScssVarDefinitionProvider implements vscode.DefinitionProvi
     }
 
     const wordRange = document.getWordRangeAtPosition(position)
-    const varName = `$${document.getText(wordRange)}`
-    const line = document.lineAt(position)
+    let varName = document.getText(wordRange)
+    if (!varName.startsWith('$')) {
+      varName = `$${varName}`
+    }
 
+    const line = document.lineAt(position)
     if (!line.text.includes(varName)) {
       return []
     }
