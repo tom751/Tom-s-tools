a8c2116da09c2d8e6700bd12e10973fe33f24016
Tom Hilton <tomhilton93@gmail.com>
2023-08-15 19:13:19 +0100

    Allow custom run test commands

--- src/commands/runTestFile.ts
+++ src/commands/runTestFile.ts
@@ -3,35 +3,49 @@ import { getDocumentFolderPath, getFileNameFromPath, getFileNameWithoutExtension
 
 export default async function runTestFile(watch = false) {
   const editor = vscode.window.activeTextEditor
-  if (editor) {
-    let fileUri = editor.document.uri
-    const fileName = getFileNameFromPath(editor.document.fileName)
+  if (!editor) {
+    vscode.window.showErrorMessage('No active text editor')
+    return
+  }
 
-    if (!fileName.endsWith('.spec.ts') && !fileName.endsWith('.spec.tsx')) {
-      // Switch to the test file
-      const folder = getDocumentFolderPath(editor.document.fileName)
-      const fileNameWithoutExtension = getFileNameWithoutExtension(fileName)
-      const pattern = new vscode.RelativePattern(folder, `${fileNameWithoutExtension}.spec.{ts,tsx}`)
+  const scriptToRun = vscode.workspace
+    .getConfiguration(watch ? 'tom.runTestFileWatch' : 'tom.runTestFile')
+    .get<string>('script')
 
-      const vals = await vscode.workspace.findFiles(pattern)
-      if (vals.length > 0) {
-        fileUri = vals[0]
-      } else {
-        vscode.window.showInformationMessage('Could not find a suitable test file to run')
-        return
-      }
-    }
+  if (!scriptToRun) {
+    vscode.window.showErrorMessage(
+      'No script to run - you need to set extension setting tom.runTestFile.script and tom.runTestFileWatch.script e.g. `npm t`',
+    )
+    return
+  }
 
-    const projectIndex = editor.document.fileName.lastIndexOf('/src/')
-    if (projectIndex === -1) {
+  let fileUri = editor.document.uri
+  const fileName = getFileNameFromPath(editor.document.fileName)
+
+  if (!fileName.endsWith('.spec.ts') && !fileName.endsWith('.spec.tsx')) {
+    // Switch to the test file
+    const folder = getDocumentFolderPath(editor.document.fileName)
+    const fileNameWithoutExtension = getFileNameWithoutExtension(fileName)
+    const pattern = new vscode.RelativePattern(folder, `${fileNameWithoutExtension}.spec.{ts,tsx}`)
+
+    const vals = await vscode.workspace.findFiles(pattern)
+    if (vals.length > 0) {
+      fileUri = vals[0]
+    } else {
+      vscode.window.showInformationMessage('Could not find a suitable test file to run')
       return
     }
-
-    const terminal = vscode.window.activeTerminal || vscode.window.createTerminal('Terminal')
-    terminal.show()
-
-    const projectPath = editor.document.fileName.slice(0, projectIndex)
-    const command = `cd ${projectPath} && npx vitest ${watch ? '' : 'run'} ${fileUri.path} `
-    terminal.sendText(command)
   }
+
+  const projectIndex = editor.document.fileName.lastIndexOf('/src/')
+  if (projectIndex === -1) {
+    return
+  }
+
+  const terminal = vscode.window.activeTerminal || vscode.window.createTerminal('Terminal')
+  terminal.show()
+
+  const projectPath = editor.document.fileName.slice(0, projectIndex)
+  const command = `cd ${projectPath} && ${scriptToRun} ${fileUri.path}`
+  terminal.sendText(command)
 }
